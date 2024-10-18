5e000e18c1e95194f1a717be9f2ef08303556200
marco.dallalba <marco.dalalba@dbserver.com.br>
2024-10-18 11:42:37 -0300

    init

---
+++ BanrisulProjetoBanco.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BanrisulProjetoBanco.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BanrisulProjetoBanco.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BanrisulProjetoBanco.Web.WebApiApplication" Language="C#" %>
